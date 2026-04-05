1242f682838c53470e293178dcb601f0ea834e18
Kauan Luiz <160810368+Kauan-Luiz@users.noreply.github.com>
2026-04-04 23:25:57 -0300

    Update config.php

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 // Configurações do Banco
 // IMPORTANTE: Em DB_HOST, coloque o que está escrito no campo "Internal URL" lá no Coolify (provavelmente é sistema_tv ou algo parecido)
-define('DB_HOST', 'sistema_tv'); 
+define('DB_HOST', 'jmdzjpkm794k8pw7hjxc174x');
 
 // IMPORTANTE: Veja se o campo "Initial Database" no Coolify está como sistema_tv ou default. Tem que ser igual.
 define('DB_NAME', 'sistema_tv'); 
@@ -11,6 +11,8 @@ define('DB_USER', 'root');
 // Aqui vai a sua senha gigante do Coolify
 define('DB_PASS', 'nxr4iPDx49EE5bv5jKn6FibkrslBGbbBFf5UbhSv47XmqJiyyAHJemYWECnh5VOj'); 
 
-// Caminhos do Sistema (Depois a gente arruma isso quando o Coolify te der um link real)
-define('BASE_URL', 'http://sua-vps-ip/public/');
+// Caminhos do Sistema
+define('BASE_URL', 'http://z9zj9djg9154zpabh3whv24b.168.119.244.43.sslip.io/public/');
+
+// Não precisa mexer nessa linha, ela já faz o cálculo da pasta perfeitamente!
 define('UPLOAD_PATH', __DIR__ . '/../public/uploads/');
