37f7c8a7310a4ce1376a80ae90cc742913bbf8c3
Kauan Luiz <160810368+Kauan-Luiz@users.noreply.github.com>
2026-04-04 23:03:17 -0300

    Update config.php

--- core/config.php
+++ core/config.php
@@ -1,10 +1,16 @@
 <?php
 // Configurações do Banco
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'sistema_tv');
-define('DB_USER', 'root'); // Troque pelos dados da sua VPS
-define('DB_PASS', '');     // Troque pela sua senha
+// IMPORTANTE: Em DB_HOST, coloque o que está escrito no campo "Internal URL" lá no Coolify (provavelmente é sistema_tv ou algo parecido)
+define('DB_HOST', 'sistema_tv'); 
 
-// Caminhos do Sistema
+// IMPORTANTE: Veja se o campo "Initial Database" no Coolify está como sistema_tv ou default. Tem que ser igual.
+define('DB_NAME', 'sistema_tv'); 
+
+define('DB_USER', 'root'); 
+
+// Aqui vai a sua senha gigante do Coolify
+define('DB_PASS', 'nxr4iPDx49EE5bv5jKn6FibkrslBGbbBFf5UbhSv47XmqJiyyAHJemYWECnh5VOj'); 
+
+// Caminhos do Sistema (Depois a gente arruma isso quando o Coolify te der um link real)
 define('BASE_URL', 'http://sua-vps-ip/public/');
 define('UPLOAD_PATH', __DIR__ . '/../public/uploads/');
